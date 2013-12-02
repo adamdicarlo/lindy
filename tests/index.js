e44c6fd1a4538976b1d684e1b97bfc9813eb0de9
Adam J. DiCarlo <adam.dicarlo@gmail.com>
2013-12-01 16:20:58 -0800

    Refactor for loop test.

--- tests/index.js
+++ tests/index.js
@@ -2,12 +2,12 @@ var lindy = require('../index')
   , test = require('tape')
 
 test('test basic for loop tag', function(assert) {
-  var rendered,
-      template = ' {% for color in colors %}\n {{ color }}\n {% endfor %} '
+  var source = ' {% for color in colors %}\n {{ color }}\n {% endfor %} '
+    , render = lindy(source)
+    , output
 
-  template = lindy(template)
-  rendered = template({colors: ['red', 'blue']})
-  assert.ok(/^\s+red\s+blue\s+$/.test(rendered), 'contains red and blue')
+  output = render({colors: ['red', 'blue']})
+  assert.ok(/^\s+red\s+blue\s+$/.test(output), 'contains red and blue')
   assert.end()
 })
 
