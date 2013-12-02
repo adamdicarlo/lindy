f6229ce9e0933b9a51d66a4ac0886cb5e08cffcd
Adam J. DiCarlo <adam.dicarlo@gmail.com>
2013-12-01 16:17:27 -0800

    Add if/else tag with initial tests.

--- tests/index.js
+++ tests/index.js
@@ -10,3 +10,23 @@ test('test basic for loop tag', function(assert) {
   assert.ok(/^\s+red\s+blue\s+$/.test(rendered), 'contains red and blue')
   assert.end()
 })
+
+test('test basic if tag', function(assert) {
+  var source = 'hello{% if specific %} {{specific}}{%endif%}'
+    , render = lindy(source)
+
+  assert.strictEqual(render(), 'hello')
+  assert.strictEqual(render({specific: 'world'}), 'hello world')
+  assert.end()
+})
+
+test('test basic if/else tag', function(assert) {
+  var source = 'hello, {% if happy %}my friend{% else %}jerk{% endif %}!'
+    , render = lindy(source)
+
+  assert.strictEqual(render(), 'hello, jerk!')
+  assert.strictEqual(render({happy: false}), 'hello, jerk!')
+  assert.strictEqual(render({happy: true}), 'hello, my friend!')
+  assert.strictEqual(render({happy: 'truthy value'}), 'hello, my friend!')
+  assert.end()
+})
